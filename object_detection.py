62d01e3d80ce9c8579924cec1b7e38191c5bfc6b
carlos-silvap <carlos.silvap@outlook.com>
2022-04-21 11:42:00 +0200

    Print count

--- object_detection.py
+++ object_detection.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 def findObject(object:string):
-    if (object == "cube"):
+    if (object == "cubes"):
         lower_range = np.array([169, 100, 100])
         upper_range = np.array([189, 255, 255])
         color = (0,255,0)
@@ -16,22 +16,23 @@ def findObject(object:string):
     mask = cv2.dilate(mask, None, iterations=2)
     cent , _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(resized,cent,-1,color,3)
-    print(len(cent))
+    count = (len(cent))
+    print(object+":",count)    
 
 
 img = cv2.imread("shapes.jpg")
 
-scale_percent = 50 # percent of original size
+#Work with the image
+scale_percent = 40 # percent of original size
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
-  
-# resize image
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
 hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
 
-findObject("cube")
-findObject("cylinder")
+findObject("cubes")
+findObject("cylinders")
 
 
 cv2.imshow("Image", resized)
